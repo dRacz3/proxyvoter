52e6b4505f7cc32016658936678136a8139f781c
dracz3 <daniel.racz@irob.uni-obuda.hu>
2020-11-03 13:33:46 +0100

    cleanup in code

--- jaspyr.py
+++ jaspyr.py
@@ -1,35 +1,56 @@
-import requests
+import logging
 import threading
-endline = '\n'
 
-with open('proxies.txt') as file:
-    proxies = [ f'http://{address.replace(endline, "")}' for address in file.readlines()]
-print(f'Loaded : {proxies}')
+import requests
 
-sucess_list = []
+from proxywrapper import get_proxies_from_file
 
-request_threads = []
-for p in proxies:
-    print(f"Voting from {p}")
-    proxy = {
-        "http": p,
-    }
-
-    iwoteforjasperlink = 'http://ebadta.hu/verseny/votegw.php?nid=779'
-    def vote():
-        try:
-            r = requests.get(iwoteforjasperlink, proxies=proxy, timeout=15)
-
-            print(r.content.decode())
-            print(r)
-            sucess_list.append(p)
-        except Exception as e:
-            print(f"Failed to vote from {p}, error : {e}")
-    t = threading.Thread(target=vote)
-    request_threads.append(t)
-    t.start()
+logger = logging.getLogger('VOTER')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s -%(threadName)s | %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
-[t.join() for t in request_threads]
-print('Finsihed voting for jasper!')
-print(sucess_list)
+def vote_link(nid):
+    return f'http://ebadta.hu/verseny/votegw.php?nid={nid}'
+
+
+def start_mass_vote(nid, proxies):
+    request_threads = []
+    success_count = 0
+    for p in proxies:
+
+        if p is None:
+            continue
+        proxy = {
+            "http": p,
+        }
+
+        def vote():
+            nonlocal success_count
+            try:
+                logger.info(f"Voting from {p} started")
+                r = requests.get(vote_link(nid), proxies=proxy, timeout=90)
+                if '1' == r.content.decode():
+                    logger.info('Vote succeeded!')
+                    success_count = success_count + 1
+                else:
+                    logger.warning(f'Voting failed with : {r.content.decode()}')
+
+            except Exception as e:
+                logger.info(f"Failed to vote from {p}, error : {e}")
+
+        t = threading.Thread(target=vote)
+        request_threads.append(t)
+        t.start()
+
+    [t.join() for t in request_threads]
+    logger.info(f'Finished, {success_count}/{len(proxies)} succeeded vote.')
+
+
+if __name__ == '__main__':
+    JASPER_NID = 779
+    start_mass_vote(JASPER_NID, get_proxies_from_file("proxies.txt"))
---
+++ proxywrapper.py
@@ -0,0 +1,23 @@
+from typing import List
+
+import requests
+import json
+
+fetch_url = 'https://gimmeproxy.com/api/getProxy?get=true&maxCheckPeriod=3600'
+
+def fetch_gimme_proxy():
+    r = requests.get(fetch_url, timeout = 2)
+    try:
+        r_content = json.loads(r.content.decode())
+        proxy_url= f'http://{r_content["ip"]}:{r_content["port"]}'
+        return proxy_url
+    except Exception as e:
+        print(f'Failed to get a proxy this time. {r.content}')
+        return None
+
+
+def get_proxies_from_file(filename : str) -> List[str]:
+    endline = '\n'
+    with open(filename) as file:
+        proxies = [f'http://{address.replace(endline, "")}' for address in file.readlines()]
+    return list(dict.fromkeys(proxies))
